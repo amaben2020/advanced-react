3ab2557847fea7999871bf8b4605a7f31a3f7cf3
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2026-03-15 10:27:08 +0100

    improving ready state

--- server/ws-echo-server.cjs
+++ server/ws-echo-server.cjs
@@ -1,6 +1,6 @@
 // Simple echo WebSocket server
 // Run: node server/ws-echo-server.cjs
-const { WebSocketServer } = require('ws');
+const { WebSocketServer, WebSocket } = require('ws'); // add WebSocket
 
 const wss = new WebSocketServer({ port: 8081 });
 console.log('Echo server running on ws://localhost:8081');
@@ -10,7 +10,19 @@ wss.on('connection', (ws) => {
 
   ws.on('message', (data) => {
     console.log('received from client', data);
+    ws.send('Echo from server 1');
+
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        // client.send(data, { binary: isBinary });
+        client.send('Client connected successfully');
+      }
+    });
   });
 
-  ws.send('Echo from server');
+  ws.on('close', () => {
+    console.log('Closing....');
+  });
+
+  // ws.send('Echo from server');
 });
--- src/components/brain-muscle/wss/TestChat.tsx
+++ src/components/brain-muscle/wss/TestChat.tsx
@@ -1,14 +1,66 @@
 import { useEffect, useRef, useState } from 'react';
 
 // ── Component ──
-export default function TestChat() {
-  let connected = false;
+export default function TestChat({ url }: { url?: string }) {
+  const [connected, setConnected] = useState(false);
+
+  const wsRef = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:8081');
+
+    wsRef.current = ws;
+
+    wsRef.current.onopen = () => {
+      setConnected(true);
+      console.log('Connected');
+    };
+
+    wsRef.current.onmessage = (event: { type: string; data: string }) => {
+      console.log('BE MESSAGE', event.data);
+    };
+
+    wsRef.current.onerror = (error) => {
+      console.log('ERROR', error);
+      setConnected(false);
+    };
+
+    wsRef.current.onclose = () => {
+      console.log('Connection closed');
+    };
+
+    return () => {
+      console.log('unmounted');
+      wsRef.current?.close();
+      setConnected(false);
+    };
+  }, []);
+
+  const handleSend = (event: any) => {
+    event.preventDefault();
+    if (wsRef.current?.readyState === WebSocket.OPEN)
+      wsRef.current?.send('Ben the great');
+  };
 
   return (
     <div
       style={{ maxWidth: 500, margin: '40px auto', fontFamily: 'monospace' }}
     >
       <h2>WSS Test Chat</h2>
+
+      <div
+        style={{
+          background: connected ? 'green' : 'red',
+          width: 8,
+          height: 8,
+          borderRadius: '999px',
+        }}
+      ></div>
+
+      <form onSubmit={handleSend}>
+        <input type="text" className="border p-3 rounded-2xl " />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
